3a1f6781c3db184de7e0e8e6484877c7ed0febf5
Chlyfen Richard <chlyfenrichard@gmail.com>
2020-08-24 09:37:33 +0800

    update model user

--- application/models/M_pengguna.php
+++ application/models/M_pengguna.php
@@ -5,9 +5,24 @@ class M_pengguna extends CI_Model{
 		$hsl=$this->db->query("SELECT tbl_pengguna.*,IF(pengguna_jenkel='L','Laki-Laki','Perempuan') AS jenkel FROM tbl_pengguna");
 		return $hsl;	
 	}
+        
+        function get_all_pengguna_admin(){
+		$hsl=$this->db->query("SELECT tbl_admin.*,IF(admin_jenkel='L','Laki-Laki','Perempuan') AS jenkel FROM tbl_admin");
+		return $hsl;	
+	}
 
 	function simpan_pengguna($nama,$jenkel,$username,$password,$email,$nohp,$level,$gambar){
 		$hsl=$this->db->query("INSERT INTO tbl_pengguna (pengguna_nama,pengguna_jenkel,pengguna_username,pengguna_password,pengguna_email,pengguna_nohp,pengguna_level,pengguna_photo) VALUES ('$nama','$jenkel','$username',md5('$password'),'$email','$nohp','$level','$gambar')");
+		return $hsl;
+	}
+        
+        function simpan_pengguna_admin($nama,$jenkel,$username,$password,$email,$nohp,$level,$gambar){
+		$hsl=$this->db->query("INSERT INTO tbl_admin (admin_nama,admin_jenkel,admin_username,admin_password,admin_email,admin_nohp,admin_level,admin_photo) VALUES ('$nama','$jenkel','$username',md5('$password'),'$email','$nohp','$level','$gambar')");
+		return $hsl;
+	}
+        
+        function simpan_pengguna_tanpa_gambar_admin($nama,$jenkel,$username,$password,$email,$nohp,$level){
+		$hsl=$this->db->query("INSERT INTO tbl_admin (admin_nama,admin_jenkel,admin_username,admin_password,admin_email,admin_nohp,admin_level) VALUES ('$nama','$jenkel','$username',md5('$password'),'$email','$nohp','$level')");
 		return $hsl;
 	}
 
@@ -15,6 +30,30 @@ class M_pengguna extends CI_Model{
 		$hsl=$this->db->query("INSERT INTO tbl_pengguna (pengguna_nama,pengguna_jenkel,pengguna_username,pengguna_password,pengguna_email,pengguna_nohp,pengguna_level) VALUES ('$nama','$jenkel','$username',md5('$password'),'$email','$nohp','$level')");
 		return $hsl;
 	}
+        
+        //update admin
+        function update_pengguna_admin($kode,$nama,$jenkel,$username,$password,$email,$nohp,$level,$gambar){
+		$hsl=$this->db->query("UPDATE tbl_admin set admin_nama='$nama',admin_jenkel='$jenkel',admin_username='$username',admin_password=md5('$password'),admin_email='$email',admin_nohp='$nohp',admin_level='$level',admin_photo='$gambar' where admin_id='$kode'");
+		return $hsl;
+	}
+        
+       
+        
+        function update_pengguna_tanpa_pass_admin($kode,$nama,$jenkel,$username,$password,$email,$nohp,$level,$gambar){
+		$hsl=$this->db->query("UPDATE tbl_admin set admin_nama='$nama',admin_jenkel='$jenkel',admin_username='$username',admin_email='$email',admin_nohp='$nohp',admin_level='$level',admin_photo='$gambar' where admin_id='$kode'");
+		return $hsl;
+	}
+        
+        function update_pengguna_tanpa_gambar_admin($kode,$nama,$jenkel,$username,$password,$email,$nohp,$level){
+		$hsl=$this->db->query("UPDATE tbl_pengguna set pengguna_nama='$nama',pengguna_jenkel='$jenkel',pengguna_username='$username',pengguna_password=md5('$password'),pengguna_email='$email',pengguna_nohp='$nohp',pengguna_level='$level' where pengguna_id='$kode'");
+		return $hsl;
+	}
+        
+        function update_pengguna_tanpa_pass_dan_gambar_admin($kode,$nama,$jenkel,$username,$password,$email,$nohp,$level){
+		$hsl=$this->db->query("UPDATE tbl_admin set admin_nama='$nama',admin_jenkel='$jenkel',admin_username='$username',admin_email='$email',admin_nohp='$nohp',admin_level='$level' where admin_id='$kode'");
+		return $hsl;
+	}
+        //end update admin function
 
 	//UPDATE PENGGUNA //
 	function update_pengguna_tanpa_pass($kode,$nama,$jenkel,$username,$password,$email,$nohp,$level,$gambar){
@@ -34,6 +73,10 @@ class M_pengguna extends CI_Model{
 		$hsl=$this->db->query("UPDATE tbl_pengguna set pengguna_nama='$nama',pengguna_jenkel='$jenkel',pengguna_username='$username',pengguna_password=md5('$password'),pengguna_email='$email',pengguna_nohp='$nohp',pengguna_level='$level' where pengguna_id='$kode'");
 		return $hsl;
 	}
+        function hapus_pengguna_admin($kode){
+		$hsl=$this->db->query("DELETE FROM tbl_admin where admin_id='$kode'");
+		return $hsl;
+	}
 	//END UPDATE PENGGUNA//
 
 	function hapus_pengguna($kode){
@@ -44,6 +87,12 @@ class M_pengguna extends CI_Model{
 		$hsl=$this->db->query("SELECT * FROM tbl_pengguna where pengguna_id='$id'");
 		return $hsl;
 	}
+        
+        function getusername_admin($id){
+		$hsl=$this->db->query("SELECT * FROM tbl_admin where admin_id='$id'");
+		return $hsl;
+	}
+        
 	function resetpass($id,$pass){
 		$hsl=$this->db->query("UPDATE tbl_pengguna set pengguna_password=md5('$pass') where pengguna_id='$id'");
 		return $hsl;
@@ -51,6 +100,11 @@ class M_pengguna extends CI_Model{
 
 	function get_pengguna_login($kode){
 		$hsl=$this->db->query("SELECT * FROM tbl_pengguna where pengguna_id='$kode'");
+		return $hsl;
+	}
+        
+        function get_pengguna_login_admin($kode){
+		$hsl=$this->db->query("SELECT * FROM tbl_admin where admin_id='$kode'");
 		return $hsl;
 	}
 
