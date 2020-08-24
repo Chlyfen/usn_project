3cb968c860d1c27187c91750d3ba754812b9035e
Chlyfen Richard <Chlyfen Richard@DESKTOP-QIRVVCA>
2020-08-24 09:24:30 +0800

    update view saat home user

--- application/views/v_dashboard.php
+++ application/views/v_dashboard.php
@@ -1,189 +1,95 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>E-Library</title>
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-	<script type="text/javascript" src="js/jquery.js"></script>
-	<script type="text/javascript" src="js/bootstrap.js"></script>
-</head>
-<body>	
-	<!-- membuat menu navigasi -->
-	<nav class="navbar navbar-default">
-		<div class="container">
-			<!-- Brand and toggle get grouped for better mobile display -->
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-					<span class="sr-only">Toggle navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand" href="https://www.malasngoding.com">E-Library</a>
-			</div>
-			
-			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-				<ul class="nav navbar-nav">
-					<li><a href="#">Home <span class="sr-only">(current)</span></a></li>
-					<li><a href="#">About Us</a></li>
-				</ul>
-			<!-- Tombol Search -->
-				<form class="navbar-form navbar-left" role="search">
-  					<div class="form-group">
-   					 	<input type="text" class="form-control" placeholder="Search">
-  					</div>
-  						<button type="submit" class="btn btn-default">GO!</button>
-				</form>
-			<!-- Akhir Tombol Search -->
-				<ul class="nav navbar-nav navbar-right">
-					<li><a href="#" data-toggle="modal" data-target="#modal-login">Login</a></li>					
-					<li><button type="button" class="btn btn-primary navbar-btn" data-toggle="modal" data-target="#modal-daftar">Daftar</button></li>
-				</ul>
-			</div><!-- /.navbar-collapse -->
-		</div><!-- /.container-fluid -->
-	</nav>	
+<?php include'v_header.php' ?>
 
-	<!-- Awal Modal login -->
-	<div class="modal fade" id="modal-login" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-		<div class="modal-dialog" role="document">
-			<form action="cek_login.php" method="post">
-			<div class="modal-content">
-				<div class="modal-header">
-					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-					<h4 class="modal-title" id="myModalLabel">Modal Login</h4>
-				</div>
-				<div class="modal-body">
-					
-						<div class="form-group">
-							<label>Email</label>
-							<input type="text" class="form-control" name="email">
-						</div>
-						<div class="form-group">
-							<label>Password</label>
-							<input type="password" class="form-control" name="password">
-						</div>						
-				
-				</div>
-				<div class="modal-footer">
-					<button type="button" class="btn btn-default" data-dismiss="modal">Batal</button>
-					<button type="button" class="btn btn-primary">Login</button>
-				</div>
-			</div>
-		</form>
-		</div>
-	</div>
-	<!-- akhir modal login-->
-	<div class="modal fade" id="modal-daftar" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-		<div class="modal-dialog" role="document">
-			<div class="modal-content">
-				<div class="modal-header">
-					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-					<h4 class="modal-title" id="myModalLabel">Modal Daftar</h4>
-				</div>
-				<div class="modal-body">
-					<form>
-						<div class="form-group">
-							<label>Nama</label>
-							<input type="text" class="form-control">
-						</div>
-						<div class="form-group">
-							<label>Email</label>
-							<input type="text" class="form-control">
-						</div>
-						<div class="form-group">
-							<label>Password</label>
-							<input type="password" class="form-control">
-						</div>						
-					</form>
-				</div>
-				<div class="modal-footer">
-					<button type="button" class="btn btn-default" data-dismiss="modal">Batal</button>
-					<button type="button" class="btn btn-primary">Daftar</button>
-				</div>
-			</div>
-		</div>
-	</div>
-	<!-- akhir modal -->
 
-	<!-- akhir menu navigasi -->
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title>E-library</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="" />
+<meta name="keywords" content="" />
+<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+<link rel="shorcut icon" type="text/css" href="<?php echo base_url() . 'assets/images/usn.png' ?>">
+<link rel="stylesheet" type="text/css" href="<?php echo base_url() . 'theme/css/bootstrap.css' ?>">
+<link rel="stylesheet" href="<?php echo base_url() . 'theme/css/style.css' ?>">
+<link rel="stylesheet" href="<?php echo base_url() . 'theme/css/animate.css' ?>">
+<link rel="stylesheet" href="<?php echo base_url() . 'theme/css/icomoon.css' ?>">
+<link rel="stylesheet" href="<?php echo base_url() . 'theme/css/bootstrap.css' ?>">
+<link rel="stylesheet" href="<?php echo base_url() . 'theme/css/flexslider.css' ?>">
+<script type="text/javascript" src="<?php echo base_url() . 'theme/js/jquery.js' ?>"></script>
+<script type="text/javascript" src="<?php echo base_url() . 'theme/js/bootstrap.js' ?>"></script>
+<script src="<?php echo base_url() . 'theme/js/jquery.min.js' ?>"></script>
+<script src="<?php echo base_url() . 'theme/js/jquery.waypoints.min.js' ?>"></script>
+<script src="<?php echo base_url() . 'theme/js/bootstrap.min.js' ?>"></script>
+<script src="<?php echo base_url() . 'theme/js/main.js' ?>"></script>
 
-	<div class="container">			
-		<!-- membuat jumbotron -->
-		<div class="jumbotron">
-			<center>			
-				<h2>Selamat datang di E-Library Fakultas Teknologi Informasi <h2>
-					<h2>Universitas Sembilanbelas November Kolaka</h2>
-				<p>Website Buku Elektronik Yang Dapat Diunduh Langsung !!</p><br/><br/>
-				<p><a class="btn btn-primary btn-lg" href="#" role="button">Mulai Belajar !</a></p>
-			</center>
-		</div>
-		<!-- akhir jumbotron -->
-		<div class="col-sm-6 col-md-3">
-			<div class="thumbnail">
-				<img src="gambar/book.jpg" alt="...">
-				<div class="caption">
-					<h3>Tutorial JQuery</h3>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-					quis nostrud exercitation ullamco laboris</p>
-				</div>
-			</div>
-		</div>
+<div class="container">
+    <div class="jumbotron text-center">
+        <div class="col-sm-15 mx-auto">
+            <h1>Selamat datang!</h1>
+            <p>Ini merupakan website <b>Elektronik Library</b> Fakultas Teknologi Informasi <br> Universitas Sembilanbelas November Kolaka.</p> </br>
+            <p>Anda telah login sebagai <b><?php echo $this->session->userdata('username'); ?></b> [admin].
+            </p>
+        </div>
+    </div>
 
-		<div class="col-sm-6 col-md-3">
-			<div class="thumbnail">
-				<img src="gambar/Book - Copy.jpg" alt="...">
-				<div class="caption">
-					<h3>Tutorial Ionic</h3>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-					quis nostrud exercitation ullamco laboris</p>
-				</div>
-			</div>
-		</div>
+    <div class="album py-5 bg-light">
+<!--        <form>
+            <div class="form-group">
+                <label>Category</label>
+                <select class="form-control" name="category" id="category" required>
+                    <?php
+//                    $no = 0;
+//                    foreach ($kat->result_array() as $i) :
+//                        $no++;
+//                        $kategori_id = $i['kategori_id'];
+//                        $kategori_nama = $i['kategori_nama'];
+                        ?>
+                        <option value="<?php //echo $kategori_id; ?>"><?php //echo $kategori_nama; ?></option>
+                    <?php //endforeach; ?>
+                </select>
+            </div>
 
-		<div class="col-sm-6 col-md-3">
-			<div class="thumbnail">
-				<img src="gambar/Book - Copy (3).jpg" alt="...">
-				<div class="caption">
-					<h3>Tutorial CodeIgniter</h3>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-					quis nostrud exercitation ullamco laboris</p>
-				</div>
-			</div>
-		</div>
+        </form>-->
+        <div class="container">
+            <div class="row">
+                <?php
+                foreach ($data->result_array() as $j) :
+                    $post_id = $j['tulisan_id'];
+                    $post_judul = $j['tulisan_judul'];
+                    $post_isi = $j['tulisan_isi'];
+                    $post_author = $j['tulisan_author'];
+                    $post_image = $j['tulisan_gambar'];
+                    $post_tglpost = $j['tanggal'];
+                    $post_slug = $j['tulisan_slug'];
+                    ?>
 
-		<div class="col-sm-6 col-md-3">
-			<div class="thumbnail">
-				<img src="gambar/Book - Copy (4).jpg" alt="...">
-				<div class="caption">
-					<h3>Tutorial Bootstrap</h3>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-						quis nostrud exercitation ullamco laboris</p>		
-				</div>
-			</div>
-		</div>
 
-	</div>
-	<br/>
-	
-	
-	<div class="clearfix"></div>
-		
-	<nav class="navbar navbar-default" style="bottom: 0;margin: 0">
-		<div class="container">	
-			<center>
-				
-			<ul class="nav navbar-nav">
-				<li><a href="#">Copyright @ FTI USN Kolaka. All rights reserved.</a></li>				
-			</ul>
+                    <div class="col-md-4">
+                        <div class="card mb-4 shadow-sm">
+                            <div class="thumbnail">
+                                <img src="<?php echo base_url() . 'assets/images/' . $post_image; ?>" alt="" class="img-responsive">
+                                <div class="card-body">
+                                    <p><b> <center> Judul : <?php echo $post_judul ?></center></b></p>  
+                                    <p><?php echo $post_isi ?></em></p>
+                                    <div class="d-flex justify-content-between align-items-center">
+                                        <div class="btn-group">
+                                            <a href="<?php echo $post_slug; ?>" class="btn btn-primary" role="button">Download</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>    
+                        </div>
+                    </div>
 
-			<ul class="nav navbar-nav navbar-right">
-				<li><a href="#">Develop by Suharsono</a></li>									
-			</ul>
-			</center>		
-		</div>
-	</nav>
-</body>
-</html>
+                <?php endforeach; ?>
+            </div>
+            <!--               <nav aria-label="Page navigation">
+                               
+                           </nav>-->
+        </div>
+    </div>
+</div>
+</main>
+<br/>
+<div class="clearfix"></div>
+<?php include'v_footer.php' ?>
